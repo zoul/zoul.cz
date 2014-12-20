c487534fd4db32a674171e77d19e55862c56e6ab
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-12-20 12:41:33 +0100

    Added a first version of the traffic light phase.

---
+++ lights/code.js
@@ -0,0 +1,104 @@
+var vis = (function() {
+
+    var stateKey, eventKey, keys = {
+        hidden: "visibilitychange",
+        webkitHidden: "webkitvisibilitychange",
+        mozHidden: "mozvisibilitychange",
+        msHidden: "msvisibilitychange"
+    };
+
+    for (stateKey in keys) {
+        if (stateKey in document) {
+            eventKey = keys[stateKey];
+            break;
+        }
+    }
+
+    return function(c) {
+        if (c) document.addEventListener(eventKey, c);
+        return !document[stateKey];
+    }
+})();
+
+function Clicker(interval, volume) {
+
+    var timerID = -1;
+    var sample = new Audio('click.wav');
+
+    sample.volume = volume || 1;
+
+    this.play = function() {
+        if (timerID == -1) {
+            timerID = window.setInterval(function() {
+                sample.play();
+            }, interval || 500);
+        }
+    };
+
+    this.stop = function() {
+        if (timerID != -1) {
+            window.clearInterval(timerID);
+            timerID = -1;
+        }
+    };
+}
+
+function Timer(interval, volume) {
+
+    var slowClicker = new Clicker(interval, volume);
+    var fastClicker = new Clicker(interval/3, volume);
+    var toggleInterval = Math.floor(Math.random() * 30 + 20);
+    var timerID = -1;
+
+    var log = function(msg) {
+        console.log('Timer (@' + interval + ', toggle ' + toggleInterval + 's): ' + msg);
+    };
+
+    var slowPhase = function() {
+        log('starting slow phase.');
+        slowClicker.play();
+        timerID = window.setTimeout(function() {
+            slowClicker.stop();
+            fastPhase();
+        }, toggleInterval*1000);
+    };
+
+    var fastPhase = function() {
+        log('starting fast phase.');
+        fastClicker.play();
+        timerID = window.setTimeout(function() {
+            fastClicker.stop();
+            slowPhase();
+        }, toggleInterval*1000);
+    };
+
+    this.play = function() {
+        var startDelay = Math.random() * 1000;
+        window.setTimeout(function() {
+            slowPhase();
+        }, startDelay);
+    };
+
+    this.stop = function() {
+        window.clearTimeout(timerID);
+        slowClicker.stop();
+        fastClicker.stop();
+        log('stopped.');
+    };
+}
+
+window.onload = function() {
+    var timer1 = new Timer(800, 1.0);
+    var timer2 = new Timer(790, 0.3);
+    timer1.play();
+    timer2.play();
+    vis(function() {
+        if (vis()) {
+            timer1.play();
+            timer2.play();
+        } else {
+            timer1.stop();
+            timer2.stop();
+        }
+    });
+};
