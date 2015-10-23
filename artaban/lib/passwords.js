7e7bc1cb326d8761292633b1edff9871a8b7f6b4
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-10-23 15:01:14 +0200

    Zavedl jsem kódy stanovišť.

--- artaban/lib/passwords.js
+++ artaban/lib/passwords.js
@@ -1,15 +1,17 @@
 function submitPassword() {
-    var pwd = document.getElementById("password").value;
-    var solutionURL = "solutions/" + md5(pwd) + ".html";
+    var checkpointCode = document.getElementById("checkpoint").value;
+    var password = document.getElementById("password").value;
+    var token = checkpointCode + "#" + password;
+    var solutionURL = "solutions/" + md5(token) + ".html";
     var container = $('.output');
-    container.empty();
+    container.html('Zkouším…');
     jQuery.ajax({
         url: solutionURL,
         success: function(data) {
             container.html(data);
         },
         error: function() {
-            container.html('Nene.');
+            container.html('Máte špatně kód stanoviště, heslo, nebo obojí.');
         },
     });
     return false;
