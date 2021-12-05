d51c38bee00ee778cac91a2cae584632cfaa8e9d
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-12-05 16:58:10 +0100

    Add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h1>404: stránka nenalezena</h1>
+      <p>Ach, není, není tu, co by mě těšilo ach, není tu, není, co mě těší.</p>
+      <p>Co mě těšívalo, vodou uplynulo ach, není tu, není, co mě těší!</p>
+      <p>
+        Vždycky mně dávají, co se mně nelíbí vždycky mně dávají, co já nechci.
+      </p>
+      <p>
+        Dávají mně vdovce, ten má jen půl srdce ach, není tu, není, co mě těší.
+      </p>
+      <p>Ach, není, není tu, co by mě těšilo ach, není, není tu, co mě těší!</p>
+    </div>
+  );
+}
