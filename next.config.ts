feebd5a29119c09ae8a0fffc9cc1758031cf24bd
Tomáš Znamenáček <zoul@zoul.cz>
2026-08-03 11:20:28 +0200

    Use experimental Next TS7 compatibility flag

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  experimental: {
+    useTypeScriptCli: true,
+  },
 };
 
 export default nextConfig;
